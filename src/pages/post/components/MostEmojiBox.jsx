45618a2b30f54ede87c32bf5bd6752a37c70880c
Rhajiit <ksd9638@gmail.com>
2024-02-29 15:19:01 +0900

    ♻️ 이모지 배열 리스트를 받아 가장 많이 사용한 이모지 세개를 골라내도록 설정

--- src/pages/post/components/MostEmojiBox.jsx
+++ src/pages/post/components/MostEmojiBox.jsx
@@ -1,27 +1,38 @@
 import styles from "./MostEmojiBox.module.css";
-import EmojiPicker from "emoji-picker-react";
+
+const reactionEmojiListAll = [
+	{ emoji: "👍", count: 24 },
+	{ emoji: "😍", count: 16 },
+	{ emoji: "🎉", count: 10 },
+	{ emoji: "😂", count: 20 },
+	{ emoji: "🥰", count: 25 },
+	{ emoji: "🙄", count: 45 },
+	{ emoji: "😫", count: 4 },
+	{ emoji: "🤐", count: 10 },
+	{ emoji: "🤐", count: 9 },
+];
+
 const MostEmojiBox = () => {
-  return (
-    <>
-      <div className={styles["main-wrapper"]}>
-        <div className={styles["most-emoji-box"]}>
-          <p className={styles["most-emoji"]}>👍</p>
-          <p className='font-16-regular'>24</p>
-        </div>
-        <div className={styles["most-emoji-box"]}>
-          <p className={styles["most-emoji"]}>😍</p>
-          <p className='font-16-regular'>16</p>
-        </div>
-        <div className={styles["most-emoji-box"]}>
-          <p className={styles["most-emoji"]}>🎉</p>
-          <p className='font-16-regular'>10</p>
-        </div>
-        <button className={styles["emoji-picker-dropdown-button"]}>
-          <img src='/assets/emoji_picker_dropdown_icon.svg' />
-        </button>
-      </div>
-    </>
-  );
+	const reactionEmojiList = reactionEmojiListAll
+		.sort((a, b) => b.count - a.count)
+		.filter((item, i) => i < 8);
+	const mostUsedEmoji = reactionEmojiList.filter((item, i) => i < 3);
+
+	return (
+		<>
+			<div className={styles["main-wrapper"]}>
+				{mostUsedEmoji.map((item) => (
+					<div key={item.emoji} className={styles["most-emoji-box"]}>
+						<p className={styles["most-emoji"]}>{item.emoji}</p>
+						<p className='font-16-regular'>{item.count}</p>
+					</div>
+				))}
+				<button className={styles["emoji-picker-dropdown-button"]}>
+					<img src='/assets/emoji_picker_dropdown_icon.svg' />
+				</button>
+			</div>
+		</>
+	);
 };
 
 export default MostEmojiBox;
