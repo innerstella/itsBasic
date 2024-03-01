c37862cdb5691067cdfdd7a2d9033939f0ea33e6
Rhajiit <155033024+Rhajiit@users.noreply.github.com>
2024-03-01 13:13:30 +0900

    ✅ 스타일 재조정 완료. 기존 파일의 주석 처리 해제

--- src/pages/post/components/postCard/PostCardItem.jsx
+++ src/pages/post/components/postCard/PostCardItem.jsx
@@ -19,28 +19,28 @@ export function PostCardItem() {
 
 	return (
 		<>
-			{/* {cardData.map((el) => (
-        <S.CardItem key={el.id}>
-          <S.CardHeader>
-            <S.ProfileImage
-              src={el.profileImageURL}
-              alt='이미지'
-            ></S.ProfileImage>
-            <S.CardHeaderContainer>
-              <S.CardHeaderName>
-                <S.From>From.</S.From>
-                <S.Name>{el.sender}</S.Name>
-              </S.CardHeaderName>
-              <S.Relationship>가족</S.Relationship>
-            </S.CardHeaderContainer>
-            <S.DeleteButton>
-              <img src={`/assets/post/deleted.svg`} alt='삭제휴지통'></img>
-            </S.DeleteButton>
-          </S.CardHeader>
-          <S.Content fontFamily={el.font}>{el.content}</S.Content>
-          <S.Data>{formatDate(el.createdAt)}</S.Data>
-        </S.CardItem>
-      ))} */}
+			{cardData.map((el) => (
+				<S.CardItem key={el.id}>
+					<S.CardHeader>
+						<S.ProfileImage
+							src={el.profileImageURL}
+							alt='이미지'
+						></S.ProfileImage>
+						<S.CardHeaderContainer>
+							<S.CardHeaderName>
+								<S.From>From.</S.From>
+								<S.Name>{el.sender}</S.Name>
+							</S.CardHeaderName>
+							<S.Relationship>가족</S.Relationship>
+						</S.CardHeaderContainer>
+						<S.DeleteButton>
+							<img src={`/assets/post/deleted.svg`} alt='삭제휴지통'></img>
+						</S.DeleteButton>
+					</S.CardHeader>
+					<S.Content fontFamily={el.font}>{el.content}</S.Content>
+					<S.Data>{formatDate(el.createdAt)}</S.Data>
+				</S.CardItem>
+			))}
 		</>
 	);
 }
