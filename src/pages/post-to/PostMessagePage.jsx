08ab8a93ef21d3d0736707a3acafc46bb8754903
innerstella <suji0509@naver.com>
2024-03-02 13:04:42 +0900

    🚧 wip

--- src/pages/post-to/PostMessagePage.jsx
+++ src/pages/post-to/PostMessagePage.jsx
@@ -1,12 +1,16 @@
+import NavigationBar from "../../components/navigationBar/NavigationBar";
+import * as S from "./PostMessagePage.style";
+
 /**
  *
  * @description 롤링 페이퍼에 메시지 보내기 페이지 (/post/{id}/message)
  */
 const PostMessagePage = () => {
   return (
-    <div>
+    <S.Container>
+      <NavigationBar />
       <h1>PostMessagePage</h1>
-    </div>
+    </S.Container>
   );
 };
 
