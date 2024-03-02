7bf78d5736a8dc9259f6cccae8a7ef5741d7d409
Rhajiit <155033024+Rhajiit@users.noreply.github.com>
2024-03-02 17:49:30 +0900

    ✨ HeaderAddEmojiButton 이모지 선택시 서버 등록 기능 추가

--- src/pages/post/components/HeaderAddEmojiButton.jsx
+++ src/pages/post/components/HeaderAddEmojiButton.jsx
@@ -2,9 +2,30 @@ import * as S from "./HeaderAddEmojiButton.style";
 import EmojiPicker from "emoji-picker-react";
 import { useState } from "react";
 import DropdownClickCancel from ".//DropdownClickCancel";
+import { useParams } from "react-router";
 
 function HeaderAddEmojiButton() {
 	const [isEmojiOpen, setIsEmojiOpen] = useState(false);
+	const { recipientId } = useParams();
+
+	const handleEmojiSelect = async (s) => {
+		try {
+			await fetch(
+				`https://rolling-api.vercel.app/4-2/recipients/${recipientId}/reactions/`,
+				{
+					method: "POST",
+					headers: {
+						accept: "application/json",
+						"Content-Type": "application/json",
+						"X-CSRFToken":
+							"Bk3gqgI4mVP95yjXHakJ56YvHIICSlhOI4lQEztPAT734s9WjGvk04ga24gCLkb6",
+					},
+					body: JSON.stringify({ emoji: s.emoji, type: "increase" }),
+				}
+			);
+		} finally {
+		}
+	};
 
 	return (
 		<S.EmojiToggleBtnContainer>
@@ -16,9 +37,18 @@ function HeaderAddEmojiButton() {
 				추가
 			</S.Button>
 			<S.EmojiPickerPositioner>
-				{isEmojiOpen && <EmojiPicker emojiStyle='twitter' />}
+				{isEmojiOpen && (
+					<EmojiPicker
+						emojiStyle='twitter'
+						onEmojiClick={(select) => handleEmojiSelect(select)}
+					/>
+				)}
 			</S.EmojiPickerPositioner>
-			<DropdownClickCancel isOpen={isEmojiOpen} setIsOpen={setIsEmojiOpen} />
+			<DropdownClickCancel
+				isOpen={isEmojiOpen}
+				recipientId={recipientId}
+				setIsOpen={setIsEmojiOpen}
+			/>
 		</S.EmojiToggleBtnContainer>
 	);
 }
