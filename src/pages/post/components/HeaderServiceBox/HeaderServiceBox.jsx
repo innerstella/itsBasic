7857a0ddc94498e291cd69f8f17943f2208ec20f
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2024-03-04 17:38:57 +0900

    ✨ Post 페이지 header 부분에서 몇개의 페이퍼가 있는지 표시하는 기능 추가

--- src/pages/post/components/HeaderServiceBox/HeaderServiceBox.jsx
+++ src/pages/post/components/HeaderServiceBox/HeaderServiceBox.jsx
@@ -6,40 +6,40 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const fetchEmojiData = async (recipientId) => {
-	const { results } = await (
-		await fetch(
-			`https://rolling-api.vercel.app/4-2/recipients/${recipientId}/reactions/`
-		)
-	).json();
-	return results;
+  const { results } = await (
+    await fetch(
+      `https://rolling-api.vercel.app/4-2/recipients/${recipientId}/reactions/`
+    )
+  ).json();
+  return results;
 };
 
 const HeaderServiceBox = () => {
-	const [emojiData, setEmojiData] = useState([]);
-	const { recipientId } = useParams();
+  const [emojiData, setEmojiData] = useState([]);
+  const { recipientId } = useParams();
 
-	const handleSetEmojiData = async (recipientId) => {
-		setEmojiData([]);
-		try {
-			const emojiRaw = await fetchEmojiData(recipientId);
-			if (!emojiRaw) {
-				throw new Error("정상적인 페이지 접근이 아닙니다.");
-			}
-			setEmojiData(emojiRaw);
-		} catch {}
-	};
+  const handleSetEmojiData = async (recipientId) => {
+    setEmojiData([]);
+    try {
+      const emojiRaw = await fetchEmojiData(recipientId);
+      if (!emojiRaw) {
+        throw new Error("정상적인 페이지 접근이 아닙니다.");
+      }
+      setEmojiData(emojiRaw);
+    } catch {}
+  };
 
-	useEffect(() => {
-		handleSetEmojiData(recipientId);
-	}, [recipientId]);
+  useEffect(() => {
+    handleSetEmojiData(recipientId);
+  }, [recipientId]);
 
-	return (
-		<S.Container>
-			<MessageWriterBox />
-			<MostEmojiBox emojiData={emojiData} emojiFunc={handleSetEmojiData} />
-			<HeaderButtonBox emojiFunc={handleSetEmojiData} />
-		</S.Container>
-	);
+  return (
+    <S.Container>
+      <MessageWriterBox recipientId={recipientId} />
+      <MostEmojiBox emojiData={emojiData} emojiFunc={handleSetEmojiData} />
+      <HeaderButtonBox emojiFunc={handleSetEmojiData} />
+    </S.Container>
+  );
 };
 
 export default HeaderServiceBox;
--- src/pages/post/components/MessageWriterBox/MessageWriterBox.jsx
+++ src/pages/post/components/MessageWriterBox/MessageWriterBox.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import WriterImageBox from "../WriterImageBox/WriterImageBox";
 import * as S from "./MessageWriterBox.style";
-const MessageWriterBox = () => {
+
+const MessageWriterBox = ({ recipientId }) => {
+  const [data, setData] = useState({});
+  const fetchData = async () => {
+    const json = await (
+      await fetch(
+        `https://rolling-api.vercel.app/4-2/recipients/${recipientId}/`
+      )
+    ).json();
+
+    setData(json);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <S.Container>
       <WriterImageBox />
       <p className='font-18-regular'>
-        <span className='font-18-bold'>23</span>명이 작성했어요!
+        <span className='font-18-bold'>{data.messageCount}</span>명이
+        작성했어요!
       </p>
     </S.Container>
   );
