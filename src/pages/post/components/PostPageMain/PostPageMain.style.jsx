9a048d3711ca5cdaa46e5d439dca1b19b19fdfaa
Rhajiit <155033024+Rhajiit@users.noreply.github.com>
2024-03-05 17:08:31 +0900

    🐛 PostCardMain의 레이아웃의 세로 길이를 vh로 변경

--- src/pages/post/components/PostPageMain/PostPageMain.style.jsx
+++ src/pages/post/components/PostPageMain/PostPageMain.style.jsx
@@ -5,7 +5,7 @@ export const Layout = styled.div`
   background-color: var(--color-orange-300);
   padding-top: 11.4rem;
   padding-bottom: 24.6rem;
-  height: calc(100% - 6.2rem - 6.8rem);
+  height: calc(100vh - 6.2rem - 6.8rem);
   @media (max-width: 1247.5px) {
     padding: 0rem 2.4rem;
     padding-top: 11.4rem;
