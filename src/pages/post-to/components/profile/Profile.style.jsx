b9af81d872237013da3037cda592d65ad19ab1dc
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2024-03-07 19:27:21 +0900

    💄 MESSAGE 페이지 Profile 컴포넌트 모바일 사이즈 스타일 추가

--- src/pages/post-to/components/profile/Profile.style.jsx
+++ src/pages/post-to/components/profile/Profile.style.jsx
@@ -5,6 +5,10 @@ export const Container = styled.div`
   align-items: center;
   gap: 3.2rem;
 
+  @media (max-width: 767.5px) {
+    display: flex;
+    flex-direction: column;
+  }
   .selected-profile {
     border-radius: 10rem;
     width: 8rem;
@@ -17,6 +21,10 @@ export const Container = styled.div`
     gap: 1.2rem;
     .text {
       color: var(--color-grayscale-500);
+
+      @media (max-width: 767.5px) {
+        text-align: center;
+      }
     }
     .profile {
       width: 5.6rem;
@@ -26,6 +34,12 @@ export const Container = styled.div`
     .profile-container {
       display: flex;
       gap: 0.4rem;
+
+      @media (max-width: 767.5px) {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 2rem;
+      }
     }
   }
 `;
