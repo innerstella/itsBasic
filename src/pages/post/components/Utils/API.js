c3c90dcb6877ecd19daa3ea88d0f10c57dfdc61f
Rhajiit <155033024+Rhajiit@users.noreply.github.com>
2024-03-08 11:30:16 +0900

    ♻️ API의 fetchData 함수를 다른 곳에서도 활용 가능하게 고침

--- src/pages/post/components/Utils/API.js
+++ src/pages/post/components/Utils/API.js
@@ -1,16 +1,18 @@
-/**
- *
- * @returns
- * @desc recipients 전체 데이터를 가져오는 함수입니다 파라미터로 useParams를 받아 recipientId에 맞는 메세지 전체를 불러옵니다
- *
- */
-
 const BASE_URL = "https://rolling-api.vercel.app/4-2";
 
-export const FetchData = async (url) => {
-  const json = await (await fetch(url)).json();
-
-  return json;
+/**
+ * @returns
+ * @desc 서버에 대한 Request 전반을 다루는 함수입니다.
+ * @param query baseURL 에서 추가로 요청할 정보의 경로를 적습니다.
+ * @param requestInformation method가 GET이 아닐 경우 추가로 작성하여 request의 성질을 조절합니다.
+ */
+const FetchData = async (query, requestInformation = undefined) => {
+  try {
+    const json = await (await fetch(query, requestInformation)).json();
+    return json;
+  } catch (e) {
+    console.error(e.message);
+  }
 };
 
 export default FetchData;
