d5cc03555e3a9a46c97115a50a335b40c1225298
Rhajiit <155033024+Rhajiit@users.noreply.github.com>
2024-03-08 16:49:04 +0900

    💄 PostPage의 버튼들에 Hover상태 컬러 부여

--- src/pages/post/components/HeaderAddEmojiButton/HeaderAddEmojiButton.style.jsx
+++ src/pages/post/components/HeaderAddEmojiButton/HeaderAddEmojiButton.style.jsx
@@ -1,36 +1,39 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-	padding: 0.6rem 1.6rem;
-	border-radius: 0.6rem;
-	border: 1px solid var(--color-grayscale-300);
-	background: var(--color-white);
-	display: flex;
-	align-items: center;
-	gap: 0.4rem;
-	cursor: pointer;
-	color: var(--color-grayscale-900);
+  padding: 0.6rem 1.6rem;
+  border-radius: 0.6rem;
+  border: 1px solid var(--color-grayscale-300);
+  background: var(--color-white);
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+  cursor: pointer;
+  color: var(--color-grayscale-900);
 
-	@media (max-width: 767.5px) {
-		padding: 0.6rem 0.8rem;
-		font-size: 0;
-		gap: 0;
-		> img {
-			height: 2rem;
-			width: 2rem;
-		}
-	}
+  @media (max-width: 767.5px) {
+    padding: 0.6rem 0.8rem;
+    font-size: 0;
+    gap: 0;
+    > img {
+      height: 2rem;
+      width: 2rem;
+    }
+  }
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
 `;
 
 export const EmojiToggleBtnContainer = styled.div`
-	position: relative;
-	text-align: right;
-	z-index: 2;
+  position: relative;
+  text-align: right;
+  z-index: 2;
 `;
 
 export const EmojiPickerPositioner = styled.section`
-	position: absolute;
-	right: 18px;
-	transform: translateY(0.6rem);
-	z-index: 2;
+  position: absolute;
+  right: 18px;
+  transform: translateY(0.6rem);
+  z-index: 2;
 `;
