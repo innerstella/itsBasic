d6f304bfde30f372e67839a49af62ea3cfd0b63d
minho <kasamin98@naver.com>
2024-03-10 16:17:56 +0900

    💄 post 롤링페이퍼 작성된 카드 및 모달에 테마 적용

--- src/pages/post/components/RollingMessageModal/RollingMessageModal.style.jsx
+++ src/pages/post/components/RollingMessageModal/RollingMessageModal.style.jsx
@@ -18,7 +18,7 @@ export const RollingMessageModalBackground = styled.div`
 
 export const RollingMessageSender = styled.p`
   margin-bottom: 0.6rem;
-  color: #000;
+  color: ${(props) => props.theme.textColor};
   & span {
     font-weight: 700;
   }
@@ -29,7 +29,7 @@ export const RollingMessageModalWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   position: absolute;
-  background-color: var(--color-white);
+  background: ${(props) => props.theme.backgroundColor};
   transform: translateX(-50%) translateY(-50%);
   border-radius: 1.6rem;
   padding: 4rem;
@@ -45,7 +45,7 @@ export const RollingMessageModalWrapper = styled.div`
     margin-bottom: 1.9rem;
     width: 100%;
 
-    color: var(--gray-400, #999);
+    color: ${(props) => props.theme.textColor};
 
     & section {
       display: flex;
@@ -80,8 +80,8 @@ export const ModalMessageTextArea = styled.textarea`
   width: 100%;
   height: 24rem;
   margin-bottom: 2rem;
-  color: #5a5a5a;
-  background: var(--color-white);
+  color: ${(props) => props.theme.textColor};
+  background: ${(props) => props.theme.backgroundColor};
 
   &:focus {
     outline: none;
--- src/pages/post/components/postCard/PostCardItem.style.jsx
+++ src/pages/post/components/postCard/PostCardItem.style.jsx
@@ -8,7 +8,7 @@ export const CardPlus = styled.div`
   align-items: center;
   flex-shrink: 0;
   border-radius: 1.6rem;
-  background: var(--white, #fff);
+  background: ${(props) => props.theme.backgroundColor};
   box-shadow: 0px 2px 12px 0px rgba(0, 0, 0, 0.08);
   @media (max-width: 1023.5px) {
     width: 100%;
@@ -20,7 +20,7 @@ export const CardItem = styled.div`
   display: flex;
   flex-direction: column;
   border-radius: 1.6rem;
-  background: var(--white, #fff);
+  background: ${(props) => props.theme.backgroundColor};
   box-shadow: 0px 2px 12px 0px rgba(0, 0, 0, 0.08);
 
   &:hover {
@@ -47,7 +47,7 @@ export const Content = styled.p`
   width: 100%;
   height: 11.6rem;
   word-break: break-all;
-  color: var(--gray-600, #4a4a4a);
+  color: ${(props) => props.theme.textColor};
   display: -webkit-box;
   -webkit-line-clamp: 4;
   -webkit-box-orient: vertical;
@@ -77,7 +77,7 @@ export const ProfileImage = styled.img`
   background: var(--white, #fff);
 `;
 export const From = styled.p`
-  color: #000;
+  color: ${(props) => props.theme.textColor};
   font-family: Pretendard;
   font-size: 20px;
   font-style: normal;
@@ -86,7 +86,7 @@ export const From = styled.p`
 `;
 export const Name = styled.p`
   width: 14rem;
-  color: #000;
+  color: ${(props) => props.theme.textColor};
   font-family: Pretendard;
   font-size: 20px;
   font-style: normal;
