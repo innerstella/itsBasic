2558571f061d43fbaca12d4c27a356a5c0f5ceac
hoj0806 <90647684+hoj0806@users.noreply.github.com>
2024-03-11 18:21:52 +0900

    💄 Post 페이지 헤더부분 깨지는 현상 해결, 헤더부분 드롭다운 아이콘 다크모드에 맞춰 색상 변경 적용

--- src/pages/post/components/PostPageMain/PostPageMain.jsx
+++ src/pages/post/components/PostPageMain/PostPageMain.jsx
@@ -24,7 +24,7 @@ const PostPageMain = ({
 
       {currentURL.includes("edit") || (
         <ST.Box>
-          <ST.StyledLink to="edit">삭제하기</ST.StyledLink>
+          <ST.StyledLink to='edit'>삭제하기</ST.StyledLink>
         </ST.Box>
       )}
 
